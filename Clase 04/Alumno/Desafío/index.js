d4347ea2318c9e86550dbccf54e2d003c9e69374
Lumel <42525680+jlumel@users.noreply.github.com>
2019-10-05 13:38:48 -0300

    desafio04

--- Clase 04/Alumno/Desafío/index.js
+++ Clase 04/Alumno/Desafío/index.js
@@ -20,27 +20,23 @@ var studentsList = [
       dni: 45678983
     }
   ]
-let miAlumno;
-
-
   
+  let miAlumno;
 
-miAlumno = prompt("Ingrese un nombre");
-
- while (miAlumno !== "exit"){
-  
-  function miBusqueda(textoIngresado, alumnos) {
-    let posicion = -1;
-    for (let i = 0; i < alumnos.length; i++) {
-      let alumno = alumnos[i];
-          if (alumno.firstName === textoIngresado || alumno.lastName === textoIngresado)  {
-        posicion = i
-      }
+function miBusqueda(textoIngresado, alumnos) {
+  let posicion = -1;
+  for (let i = 0; i < alumnos.length; i++) {
+    let alumno = alumnos[i];
+        if (alumno.firstName === textoIngresado || alumno.lastName === textoIngresado)  {
+      posicion = i
     }
-      return posicion 
-    }
-  
-  console.log(miBusqueda(miAlumno,studentsList))
+  }
+    return posicion 
+  }
+
+do {
   miAlumno = prompt("Ingrese un nombre");
- }
 
+  console.log(miBusqueda(miAlumno,studentsList));
+
+} while (miAlumno !== "exit");
