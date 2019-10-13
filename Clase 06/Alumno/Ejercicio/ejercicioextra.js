918a67985807c53cf1e04f5c0d2a68ae78152d54
Lumel <42525680+jlumel@users.noreply.github.com>
2019-10-13 20:30:04 -0300

    clase06

--- Clase 06/Alumno/Ejercicio/ejercicioextra.js
+++ Clase 06/Alumno/Ejercicio/ejercicioextra.js
@@ -1,20 +1,21 @@
 var numbers = [2,87,5,7,55,8,11,22,53,87,4];
 
-let nuevoArray;
+let arrayPar = [];
+let arrayImpar = [];
 
-function crearArray(array,valor) {
+function crearArray(array,valor,miArray) {
 
 for (let i = 0; i < array.length; i++) {
     let nuevoValor = array[i];
-    if (nuevoValor % 2 = valor) {
-       nuevoArray.push(numbers[i]) 
+    if (nuevoValor % 2 === valor) {
+       miArray.push(numbers[i]) 
     }
 }
 
-return nuevoArray.join(" ")
+return miArray.join(" ")
 
 }
 
-console.log(crearArray(numbers, 0));
+console.log(crearArray(numbers, 0,arrayPar));
 
-console.log(crearArray(numbers, 1));
+console.log(crearArray(numbers, 1,arrayImpar));
